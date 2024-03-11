b260c02a45e731ab4b1850b41659a1f89d8c3e14
CPZhang9487 <chenpozhang@gmail.com>
2024-03-11 23:19:06 +0800

    modified:   .gitignore 	modified:   block_like/block_like_2D.gd 	modified:   block_like/block_like_2d_manager.gd 	modified:   block_like/block_like_2d_manager.tscn 	modified:   project.godot

--- block_like/block_like_2D.gd
+++ block_like/block_like_2D.gd
@@ -5,44 +5,43 @@ class_name BlockLike2D
 extends Node2D
 
 const BLOCK_SIZE = 16
-const MODULATE_ALPHA_UPDATE_SPEED = 1.0
 
 var block_area: Area2D
 
-var _is_able_to_be_placed := true:
+var is_able_to_be_placed := true:
 	set(value):
-		_is_able_to_be_placed = value
-		if not _is_able_to_be_placed:
+		is_able_to_be_placed = value
+		if not is_able_to_be_placed:
 			return
 		for overlapping_area in block_area.get_overlapping_areas():
 			if overlapping_area.is_in_group("BLOCK_AREA"):
-				_is_able_to_be_placed = false
+				is_able_to_be_placed = false
 				break
-var _is_picked_up := false:
+var is_picked_up := false:
 	set(value):
-		if not _is_selected:
+		if not is_selected:
 			value = false
-		if _is_picked_up == value:
+		if is_picked_up == value:
 			return
-		_is_picked_up = value
-		if _is_picked_up:
-			_picked_up_global_mouse_pos = get_global_mouse_position()
+		is_picked_up = value
+		if is_picked_up:
+			picked_up_global_mouse_pos = get_global_mouse_position()
 			z_index += 100
 		else:
-			position = _previous_position
+			position = previous_position
 			z_index -= 100
-var _is_selected := false:
+var is_selected := false:
 	set(value):
-		if not _is_able_to_be_placed:
+		if not is_able_to_be_placed:
 			return
-		_is_selected = value
-		if _is_selected:
+		is_selected = value
+		if is_selected:
 			return
-		_previous_position = position
-		_is_picked_up = false
-var _is_mouse_entered := false
-var _picked_up_global_mouse_pos: Vector2
-var _previous_position: Vector2
+		previous_position = position
+		is_picked_up = false
+var is_mouse_entered := false
+var picked_up_global_mouse_pos: Vector2
+var previous_position: Vector2
 
 func _ready():
 	# block_area
@@ -60,56 +59,20 @@ func _ready():
 	add_child(block_area)
 	
 	# position
-	_locate()
-	_previous_position = position
+	previous_position = position
 
-func _physics_process(delta):
-	if Input.is_action_just_released("mouse_left"):
-		if _is_mouse_entered:
-			_is_selected = not _is_selected
-		elif not Input.is_key_pressed(KEY_CTRL):
-			_is_selected = false
-	if Input.is_action_just_released("mouse_right"):
-		_is_picked_up = not _is_picked_up
-
-	_update_modulate()
-	if _is_picked_up:
-		position = _previous_position + get_global_mouse_position() - _picked_up_global_mouse_pos
-		_locate()
-
-func select():
-	pass
-
-func _locate():
-	position = Vector2i((position + position.sign() * BLOCK_SIZE / 2) / 16) * 16
 
 func _on_block_area_area_entered(area: Area2D):
-	_is_able_to_be_placed = false
-	
+	is_able_to_be_placed = false
+
+
 func _on_block_area_area_exited(area: Area2D):
-	_is_able_to_be_placed = true
+	is_able_to_be_placed = true
+
 
 func _on_block_area_mouse_entered():
-	_is_mouse_entered = true
-	
-func _on_block_area_mouse_exited():
-	_is_mouse_entered = false
+	is_mouse_entered = true
 
-func _update_modulate():
-	modulate.a = 1.0
-	modulate.r = 1.0
-	modulate.g = 1.0
-	modulate.b = 1.0
-	if not _is_selected:
-		return
-	modulate.a = abs(int(Time.get_ticks_msec() * 20 * MODULATE_ALPHA_UPDATE_SPEED / 1000) % 20 - 10) / 10.0
-	if not _is_picked_up:
-		return
-	if _is_able_to_be_placed:
-		# green
-		modulate.r = 0.0
-		modulate.b = 0.0
-	else:
-		# red
-		modulate.g = 0.0
-		modulate.b = 0.0
+
+func _on_block_area_mouse_exited():
+	is_mouse_entered = false
--- block_like/block_like_2d_manager.gd
+++ block_like/block_like_2d_manager.gd
@@ -1,15 +1,78 @@
-"
-BlockLike2D物件的管理器。
-"
 class_name BlockLike2DManager
 extends Node2D
+## 用於管理子節點中的BlockLike2D物件。
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+## BlockLike2D物件的基礎大小。
+@export var block_size := Vector2(16, 16)
+## BlockLike2D物件被選擇時的閃爍速度
+@export var modulate_alpha_update_speed := 1.0
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta: float):
+	for block_like_2d in get_children():
+		if Input.is_action_just_released("mouse_left"):
+			if block_like_2d.is_mouse_entered:
+				block_like_2d.is_selected = not block_like_2d.is_selected
+			elif not Input.is_key_pressed(KEY_CTRL):
+				block_like_2d.is_selected = false
+
+		if Input.is_action_just_released("mouse_right"):
+			block_like_2d.is_picked_up = not block_like_2d.is_picked_up
+		if block_like_2d.is_picked_up:
+			block_like_2d.position = block_like_2d.previous_position + get_global_mouse_position() - block_like_2d.picked_up_global_mouse_pos
+		
+		locate(block_like_2d)
+		update_modulate(block_like_2d)
+
+
+## 將BlockLike2D物件加到子節點中。
+func add(block_like_2d: BlockLike2D):
+	add_child(block_like_2d)
+
+
+## 定位BlockLike2D物件到正確的地方，常駐在_physics_process(delta)中呼叫。
+func locate(block_like_2d: BlockLike2D):
+	var _position = block_like_2d.position
+	var _block_size = block_like_2d.scale * block_size
+	_position.x = int((_position.x + sign(_position.x) * _block_size.x / 2) / _block_size.x) * _block_size.x
+	_position.y = int((_position.y + sign(_position.y) * _block_size.y / 2) / _block_size.y) * _block_size.y
+	block_like_2d.position = _position
+
+
+## 更新BlockLike2D物件的modulate，常駐在_physics_process(delta)中呼叫。
+func update_modulate(block_like_2d: BlockLike2D):
+	var _is_able_to_be_placed = block_like_2d.is_able_to_be_placed
+	var _is_picked_up = block_like_2d.is_picked_up
+	var _is_selected = block_like_2d.is_selected
+	var _modulate = block_like_2d.modulate
+	
+	_modulate.a = 1.0
+	_modulate.r = 1.0
+	_modulate.g = 1.0
+	_modulate.b = 1.0
+	block_like_2d.modulate = _modulate
+	
+	if not _is_selected:
+		return
+	
+	_modulate.a = abs(int(Time.get_ticks_msec() * 20 * modulate_alpha_update_speed / 1000) % 20 - 10) / 10.0
+	block_like_2d.modulate = _modulate
+	
+	if not _is_picked_up:
+		return
+	
+	if _is_able_to_be_placed:
+		# green
+		_modulate.r = 0.0
+		_modulate.b = 0.0
+	else:
+		# red
+		_modulate.g = 0.0
+		_modulate.b = 0.0
+	block_like_2d.modulate = _modulate
+
+
+## 移除子節點中的BlockLike2D物件。
+func remove(block_like_2d: BlockLike2D):
+	remove_child(block_like_2d)
